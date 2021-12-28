4b98a50ed58497ad3dd72074ddbf7e65b852bc40
Thiago Padovani <padovanitr@gmail.com>
2021-12-28 17:28:38 -0300

    turn modal into functional component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,8 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+const Input = ({ name, icon: Icon, ...rest }: any) => {
+  const inputRef = useRef<any>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
